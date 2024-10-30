616c1be582eb00062c2388014db2dbb613d1b1e6
Alexander Nikitin <mnkwork3@gmail.com>
2024-10-30 14:48:44 +0700

    Revert "save commit"
    This reverts commit 1b39d6fe948ed90d3d77657cbe913ff85eaa1095.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,15 +4,10 @@ import {NativeStackNavigationProp} from "react-native-screens/native-stack";
 import {useNavigation} from "@react-navigation/native";
 import {Button} from "@ui-kitten/components";
 import {Routers} from "../navigation/Routes.ts";
-import {useEffect} from "react";
 
 
 const Home = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>()
-
-  useEffect(() => {
-    console.log(`Home screen`)
-  }, []);
 
   return (
     <SafeAreaView style={{display: 'flex', flex: 1}}>
